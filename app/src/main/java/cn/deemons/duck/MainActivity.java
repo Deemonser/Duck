741319e63cc1e5bd9be2292040aa97b4542d76b2
Deemons <deemonsch@gmail.com>
2018-09-28 00:44:20 +0800

    获取属性

--- app/src/main/java/cn/deemons/duck/MainActivity.java
+++ app/src/main/java/cn/deemons/duck/MainActivity.java
@@ -1,17 +1,45 @@
 package cn.deemons.duck;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import cn.deemons.library.ShapeUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        LayoutInflaterCompat.setFactory2(LayoutInflater.from(this), new LayoutInflater.Factory2()
+        {
+            @Override
+            public View onCreateView(View parent, String name, Context context,
+                AttributeSet attrs) {
+                return null;
+            }
+
+            @Override
+            public View onCreateView(String name, Context context, AttributeSet attrs) {
+                Log.e(TAG, "name = " + name);
+                int n = attrs.getAttributeCount();
+                for (int i = 0; i < n; i++)
+                {
+                    Log.e(TAG, attrs.getAttributeName(i) + " , " + attrs.getAttributeValue(i));
+                }
+
+                return null;
+            }
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -36,7 +64,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        new MyFactor().onCreateView("", null, null);
+
     }
 
+    class MyFactor implements LayoutInflater.Factory {
+        @Override
+        public View onCreateView(String name, Context context, AttributeSet attrs) {
+            return null;
+        }
+    }
 
 }
